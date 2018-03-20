7092dff0864abb0caf1abbbeccbde311e44eb874
logonrm <logonrm@fiap.com.br>
2018-03-19 22:28:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ayla/fakezap/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.ayla.fakezap
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
